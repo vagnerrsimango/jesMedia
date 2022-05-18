1498e5ed93a4a6a1579a6c75033d92172bee349a
Elton Vilanculo <39657623+eltonvilanculo@users.noreply.github.com>
2022-05-18 12:46:25 +0200

    turned the page into a landing page

---
+++ assets/js/landing.js
@@ -0,0 +1,9 @@
+var $link = $('a');
+
+$link.on('click', function(event) {
+  // stop normal link function (#)
+  $link.removeClass('active');
+  $(this).addClass('active');
+});
+
+$('.default').trigger('click');
